cbea61c2b9c7eca5fd780289b304f9bee3261cbe
jhkiehna <jhkiehna@gmail.com>
2018-10-14 23:36:23 -0400

    working on candidate creation

--- app/Candidate.php
+++ app/Candidate.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Candidate extends Model
 {
     protected $table = 'candidates';
+
+    protected $fillable = [
+        'walter_id',
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'street_address_1',
+        'street_address_2',
+        'city',
+        'state',
+    ];
 }
