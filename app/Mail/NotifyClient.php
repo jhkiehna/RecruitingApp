3ca9edb796f6865e5d011347c65bf39f5406d5a1
jhkiehna <jhkiehna@gmail.com>
2018-11-04 21:36:18 -0500

    started email template

--- app/Mail/NotifyClient.php
+++ app/Mail/NotifyClient.php
@@ -6,21 +6,31 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Candidate;
+use App\Employer;
 
 class NotifyClient extends Mailable
 {
     use Queueable, SerializesModels;
+
+    protected $candidateModel;
+    protected $employerModel;
+    protected $fromAddress;
+    protected $fromName;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Candidate $candidateModel, Employer $employerModel)
     {
-        //
+        $this->candidateModel = $candidateModel;
+        $this->employerModel = $employerModel;
+        $this->fromAddress = config('mail.from.address');
+        $this->fromName = config('mail.from.name');
     }
-
+   
     /**
      * Build the message.
      *
@@ -28,6 +38,24 @@ class NotifyClient extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        //get employer Id somehow
+        //get list of candidates somehow
+        //placeholders for now until I firgure out how this info is passed in
+        $candidateIds = collect(1,2,3);
+        $employerId = 1;
+
+        $candidates = $candidateIds->map(function ($candidateId) {
+            return $this->candidateModel::findOrFail('id', $candidateId)->first()->mailTransform();
+        });
+
+        $employer = $this->employerModel::findOrFail('id', $employerId)->first();
+
+        return $this->from($this->fromAddress)
+            ->view('view.name')
+            ->text('view.name')
+            ->with([
+                'candidates' => $this->candidates,
+                'employer' => $this->employer
+            ]);
     }
 }
---
+++ resources/views/email/html/notifyClient.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html charset=UTF-8">
+    <title>Client Notification Mail</title>
+</head>
+
+<body>
+</body>
+</html>
