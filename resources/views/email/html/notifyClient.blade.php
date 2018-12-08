1def0cbc9c78d66afe480f09c5892a24cfd82827
jhkiehna <jhkiehna@gmail.com>
2018-12-08 08:33:21 -0500

    copied and cleaned up email template from cjobs. Lot of work to do on it still

--- resources/views/email/html/notifyClient.blade.php
+++ resources/views/email/html/notifyClient.blade.php
@@ -34,13 +34,6 @@
                                 <tr>
                                     <td style="word-break:break-word;font-size:0;padding:20px 25px 0;" align="left">
                                         <div class="" style="cursor:auto;color:#2b2b2b;font-family:Open Sans, Helvetica, Arial, sans-serif;font-size:14px;font-weight:300;line-height:22px;text-align:left;">
-                                            @if ($first)
-                                                Thank you for continuing to use {{ display_url() }}.  As an added benefit of choosing our site, you’ll receive weekly email alerts notifying you of any new candidates relevant to your job posting.   If you’d like to change your settings or add additional resume alerts, <a style="text-decoration: none; color: {{ site_color() }};" href="{{ link_builder(site_url() . '/company/account/alerts', $queryParameters) }}">click here</a>.
-                                            @elseif ($location)
-                                                We've identified the following @if (count($candidates) > 1) candidates which match @else candidate which matches @endif the keyword alert you set for: <span style="color:{{ site_color() }};">{{ $keywords }}</span> in <span style="color:{{ site_color() }};">{{ $location }}</span>
-                                            @else
-                                                We've identified the following @if (count($candidates) > 1) candidates which match @else candidate which matches @endif the keyword alert you set for: <span style="color:{{ site_color() }};">{{ $keywords }}</span>
-                                            @endif
                                         </div>
                                     </td>
                                 </tr>
@@ -65,44 +58,42 @@
                                     <tr>
                                         <td style="word-break:break-word;font-size:0;padding:10px 25px;" align="left">
                                             <div class="" style="cursor:auto;color:#2b2b2b;font-family:Open Sans, Helvetica, Arial, sans-serif;font-size:16px;font-weight:300;line-height:22px;text-align:left;">
-                                                <a href="{{ link_builder(site_url() . '/company/resumes/view/' . $candidate->candidate_id, $queryParameters) }}" target="_blank" style="color: inherit; text-decoration: none;">
-                                                    <p style="margin-bottom: 4px; text-transform: capitalize;">
-                                                        {{ $candidate->display_job_title }}
+                                                <p style="margin-bottom: 4px; text-transform: capitalize;">
+                                                    {{ $candidate->display_job_title }}
+                                                </p>
+                                                
+                                                <span style="font-size: 11px; font-weight: 500; color: #b2b2b2; text-transform: uppercase;">
+                                                    {{ $candidate->display_location_name }}
+                                                </span>
+                                                
+                                                @if(isset($candidate->display_experience))
+                                                    <p style="font-size: 13px; font-weight:800; margin-top: 4px; margin-bottom: 0;">
+                                                        {{$candidate->display_experience->title or 'None'}}
                                                     </p>
                                                     
-                                                    <span style="font-size: 11px; font-weight: 500; color: #b2b2b2; text-transform: uppercase;">
-                                                        {{ $candidate->display_location_name }}
+                                                    <span style="font-size: 11px; font-weight: 500; text-transform: uppercase;">
+                                                        {{$candidate->display_experience->company or 'None'}}
                                                     </span>
+                                                @endif
                                                     
-                                                    @if(isset($candidate->display_experience))
-                                                        <p style="font-size: 13px; font-weight:800; margin-top: 4px; margin-bottom: 0;">
-                                                            {{$candidate->display_experience->title or 'None'}}
-                                                        </p>
-                                                        
+                                                @if(isset($candidate->display_education))
+                                                    <p style="font-size: 13px; font-weight:800; margin-top: 4px; margin-bottom: 0;">
+                                                        {{ $candidate->display_education->school_name }}
+                                                    </p>
+                                                    
+                                                    @if($candidate->display_education->major)
                                                         <span style="font-size: 11px; font-weight: 500; text-transform: uppercase;">
-                                                            {{$candidate->display_experience->company or 'None'}}
+                                                            {{ $candidate->display_education->major }}
                                                         </span>
-                                                    @endif
                                                         
-                                                    @if(isset($candidate->display_education))
-                                                        <p style="font-size: 13px; font-weight:800; margin-top: 4px; margin-bottom: 0;">
-                                                            {{ $candidate->display_education->school_name }}
-                                                        </p>
-                                                        
-                                                        @if($candidate->display_education->major)
-                                                            <span style="font-size: 11px; font-weight: 500; text-transform: uppercase;">
-                                                                {{ $candidate->display_education->major }}
-                                                            </span>
-                                                            
-                                                        @endif
                                                     @endif
-                                                        
-                                                    @if(isset($candidate->display_summary))
-                                                        <p style="font-size: 13px; margin-top: 4px; margin-bottom: 0;">
-                                                            {{ $candidate->display_summary }}
-                                                        </p>
-                                                    @endif
-                                                </a>
+                                                @endif
+                                                    
+                                                @if(isset($candidate->display_summary))
+                                                    <p style="font-size: 13px; margin-top: 4px; margin-bottom: 0;">
+                                                        {{ $candidate->display_summary }}
+                                                    </p>
+                                                @endif
                                             </div>
                                         </td>
                                     </tr>
@@ -113,9 +104,6 @@
                                                 <tbody>
                                                     <tr>
                                                         <td style="border:1px solid {{ site_color() }};border-radius:24px;color:{{ site_color() }};cursor:auto;padding:10px 25px;" align="center" valign="middle" bgcolor="#ffffff">
-                                                            <a href="{{ link_builder(site_url() . '/company/resumes/view/' . $candidate->candidate_id, $queryParameters) }}" style="text-decoration:none;line-height:100%;background:#ffffff;color:{{ site_color() }};font-family:Open Sans, Helvetica, Arial, sans-serif;font-size:12px;font-weight:300;text-transform:uppercase;margin:0px;" target="_blank">
-                                                                View Candidate
-                                                            </a>
                                                         </td>
                                                     </tr>
                                                 </tbody>
@@ -145,21 +133,6 @@
                                                 <tbody>
                                                     <tr>
                                                         <td style="border:1px solid {{ site_color() }};border-radius:3px;color:#ffffff;cursor:auto;padding:10px 25px;" align="center" valign="middle" bgcolor="{{ site_color() }}">
-                                                            @if ($location)
-                                                                @if ($coordinates)
-                                                                    <a href="{{ link_builder(site_url() . '/company/resumes?configure[aroundLatLng]=' . $coordinates . '&location=' . $location .'&query=' . $keywords . '&referrer=' . $queryParameters->referrer) }}" style="text-decoration:none;line-height:100%;background:{{ site_color() }};color:#ffffff;font-family:Open Sans, Helvetica, Arial, sans-serif;font-size:12px;font-weight:500;text-transform:uppercase;margin:0px;" target="_blank">
-                                                                        View More Candidates
-                                                                    </a>
-                                                                @elseif ($boundingBox)
-                                                                    <a href="{{ link_builder(site_url() . '/company/resumes?configure[insideBoundingBox]=' . $boundingBox . '&location=' . $location . '&query=' . $keywords . '&referrer=' . $queryParameters->referrer) }}" style="text-decoration:none;line-height:100%;background:{{ site_color() }};color:#ffffff;font-family:Open Sans, Helvetica, Arial, sans-serif;font-size:12px;font-weight:500;text-transform:uppercase;margin:0px;" target="_blank">
-                                                                        View More Candidates
-                                                                    </a>
-                                                                @endif
-                                                            @else
-                                                                <a href="{{ link_builder(site_url() . '/company/resumes?query=' . $keywords . '&referrer=' . $queryParameters->referrer) }}" style="text-decoration:none;line-height:100%;background:{{ site_color() }};color:#ffffff;font-family:Open Sans, Helvetica, Arial, sans-serif;font-size:12px;font-weight:500;text-transform:uppercase;margin:0px;" target="_blank">
-                                                                    View More Candidates
-                                                                </a>
-                                                            @endif
                                                         </td>
                                                     </tr>
                                                 </tbody>
--- resources/views/email/partials/header.blade.php
+++ resources/views/email/partials/header.blade.php
@@ -1,10 +1,8 @@
 <!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
-    <title>{{ site_title() }}</title>
-    <!--[if !mso]><!-- -->
+    <title>{{ <?php site_title() ?> }}</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!--<![endif]-->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <style type="text/css">
         #outlook a { padding: 0; }
@@ -16,105 +14,75 @@
         img { border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; }
         p { display: block; margin: 13px 0; }
     </style>
-    <!--[if !mso]><!-->
     <style type="text/css">
         @media only screen and (max-width:480px) {
             @-ms-viewport { width:320px; }
         @viewport { width:320px; }
         }
     </style>
-    <!--<![endif]-->
-    <!--[if mso]>
-    <xml>
-        <o:OfficeDocumentSettings>
-            <o:AllowPNG/>
-            <o:PixelsPerInch>96</o:PixelsPerInch>
-        </o:OfficeDocumentSettings>
-    </xml>
-    <![endif]-->
-    <!--[if lte mso 11]>
-    <style type="text/css">
-        .outlook-group-fix {
-            width:100% !important;
-        }
-    </style>
-    <![endif]-->
-
-    <!--[if !mso]><!-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,700" rel="stylesheet" type="text/css">
     <style type="text/css">
 
         @import url(https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,700);
 
     </style>
-    <!--<![endif]-->
     <style type="text/css">
         .mj-column-per-50 { width:50%!important; }
     </style>
 </head>
+
 <body style="background: #f2f2f2;">
-<div style="background-color:#f2f2f2;"><!--[if mso | IE]>
-    <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
-        <tr>
-            <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-    <![endif]--><div style="margin:0px auto;max-width:600px;"><table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:20px 0px;"></td></tr></tbody></table></div><!--[if mso | IE]>
-    </td></tr></table>
-    <![endif]-->
-    <!--[if mso | IE]>
-    <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
-        <tr>
-            <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-    <![endif]--><div style="margin:0px auto;border-radius:3px;max-width:600px;background:#ffffff;">
+<div style="background-color:#f2f2f2;">
+    <div style="margin:0px auto;max-width:600px;">
+        <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;" align="center" border="0">
+            <tbody>
+                <tr>
+                    <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:20px 0px;">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+    <div style="margin:0px auto;border-radius:3px;max-width:600px;background:#ffffff;">
         <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;border-radius:3px;background:#ffffff;" align="center" border="0">
             <tbody>
-            <tr>
-                <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:20px 0px;padding-bottom:12px;padding-top:12px;"><!--[if mso | IE]>
-                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
-                        <tr>
-                            <td style="vertical-align:middle;width:200px;">
-                    <![endif]--><div class="mj-column-per-50 outlook-group-fix" style="vertical-align:middle;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
-                                    <table role="presentation" cellpadding="0" cellspacing="0" style="vertical-align:middle;" width="100%" border="0">
-                                        <tbody>
-                                            <tr>
-                                                <td style="word-break:break-word;font-size:0px;padding:10px 0;" align="left">
-                                                    <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px;" align="left" border="0">
-                                                        <tbody>
-                                                            <tr>
-                                                                <td>
-                                                                    <a href="{{ link_builder(site_url(), $queryParameters) }}" target="_blank"><img alt="" title="" height="70px" src="{{ site_url() }}/assets/img/{{ logo_path() }}/logo/email.png" style="border:none;display:block;outline:none;text-decoration:none;height:70px;"></a>
-                                                                </td>
-                                                            </tr>
-                                                        </tbody>
-                                                    </table>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div><!--[if mso | IE]>
-                            </td>
-                            <td style="vertical-align:middle;width:200px;">
-                            <![endif]--><div class="mj-column-per-50 outlook-group-fix" style="vertical-align:middle;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
-                                <table role="presentation" cellpadding="0" cellspacing="0" style="vertical-align:middle;" width="100%" border="0">
-                                    <tbody>
+                <tr>
+                    <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:20px 0px;padding-bottom:12px;padding-top:12px;">
+                        <div class="mj-column-per-50 outlook-group-fix" style="vertical-align:middle;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+                            <table role="presentation" cellpadding="0" cellspacing="0" style="vertical-align:middle;" width="100%" border="0">
+                                <tbody>
+                                    <tr>
+                                        <td style="word-break:break-word;font-size:0px;padding:10px 0;" align="left">
+                                            <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px;" align="left" border="0">
+                                                <tbody>
+                                                    <tr>
+                                                        <td>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        
+                        <div class="mj-column-per-50 outlook-group-fix" style="vertical-align:middle;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+                            <table role="presentation" cellpadding="0" cellspacing="0" style="vertical-align:middle;" width="100%" border="0">
+                                <tbody>
                                     <tr>
                                         <td style="word-break:break-word;font-size:0px;padding:10px 20px 0 10px;" align="right">
                                             <div class="" style="cursor:auto;color:#2b2b2b;font-family:Open Sans, Helvetica, Arial, sans-serif;font-size:14px;font-weight:300;line-height:22px;text-align:right;">
-                                                <img src="{{ site_url() }}/assets/img/{{ logo_path() }}/icons/date.png" alt="calendar" height="16" width="15" style="display:inline-block;vertical-align:middle;padding-bottom:4px;padding-right:4px;">
                                                 <span><?php echo date("F j, Y"); ?></span><br>
                                             </div>
                                         </td>
                                     </tr>
-                                    </tbody>
-                                </table>
-                                </div><!--[if mso | IE]>
-                            </td>
-                        </tr>
-                    </table>
-                    <![endif]-->
-                </td>
+                                </tbody>
+                            </table>
+                        </div>
+                    </td>
                 </tr>
             </tbody>
         </table>
-    </div><!--[if mso | IE]>
-    </td></tr></table>
-    <![endif]-->
+    </div>
