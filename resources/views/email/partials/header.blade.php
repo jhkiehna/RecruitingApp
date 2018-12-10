9a74833d358614d7c21c53ed033f14a48a7ab8e3
jhkiehna <jhkiehna@gmail.com>
2018-12-10 17:57:25 -0500

    added title to email header. Some styling still needed

--- resources/views/email/partials/header.blade.php
+++ resources/views/email/partials/header.blade.php
@@ -1,8 +1,7 @@
 <!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
-    <title>{{ <?php site_title() ?> }}</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{ site_title() }}</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <style type="text/css">
         #outlook a { padding: 0; }
@@ -28,20 +27,15 @@
     </style>
     <style type="text/css">
         .mj-column-per-50 { width:50%!important; }
+        .testLine { outline:1px solid red;}
     </style>
 </head>
 
 <body style="background: #f2f2f2;">
 <div style="background-color:#f2f2f2;">
+    <!-- Verticle Spacing div -->
     <div style="margin:0px auto;max-width:600px;">
-        <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;" align="center" border="0">
-            <tbody>
-                <tr>
-                    <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:20px 0px;">
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:20px 0px;"></td></tr></tbody></table>
     </div>
     
     <div style="margin:0px auto;border-radius:3px;max-width:600px;background:#ffffff;">
@@ -57,7 +51,8 @@
                                             <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px;" align="left" border="0">
                                                 <tbody>
                                                     <tr>
-                                                        <td>
+                                                        <td style="word-break:break-word;font-size:20px;padding:10px 20px 0 20px;" align="left">
+                                                            {{ site_title() }}
                                                         </td>
                                                     </tr>
                                                 </tbody>
@@ -72,7 +67,7 @@
                             <table role="presentation" cellpadding="0" cellspacing="0" style="vertical-align:middle;" width="100%" border="0">
                                 <tbody>
                                     <tr>
-                                        <td style="word-break:break-word;font-size:0px;padding:10px 20px 0 10px;" align="right">
+                                        <td style="word-break:break-word;font-size:0px;padding:10px 20px 0 10px;" align="left">
                                             <div class="" style="cursor:auto;color:#2b2b2b;font-family:Open Sans, Helvetica, Arial, sans-serif;font-size:14px;font-weight:300;line-height:22px;text-align:right;">
                                                 <span><?php echo date("F j, Y"); ?></span><br>
                                             </div>
