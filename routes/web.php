f13a8c7966c50c625563f6554cc1c18f59a37b9a
Minty <jhkiehna@gmail.com>
2019-01-07 00:58:31 -0500

    removed password reset routes

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,6 @@
 |
 */
 
-//Auth routes
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -51,3 +41,14 @@ Route::prefix('/dashboard')->middleware('auth')->group(function () {
     Route::post('/emailEmployer/{employerId}', 'EmployerEmailController@send')->name('email.employer');
     Route::post('/previewEmailEmployer/{employerId}', 'EmployerEmailController@preview')->name('previewEmail.employer');
 });
+
+//Auth routes
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+// Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+// Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+// Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+
